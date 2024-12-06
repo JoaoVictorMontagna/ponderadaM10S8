4503c7ae0d1958f9853d91557204cf73fdfa5f3b
JoaoVictorMontagna <Joao.Montagna@sou.inteli.edu.br>
2024-12-06 10:11:23 -0300

    Atualização do codigo para ao menos coletar dados

--- WebMetric/obj/Debug/net8.0/WebMetric.AssemblyInfo.cs
+++ WebMetric/obj/Debug/net8.0/WebMetric.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebMetric")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a384cecacb531a957cd6e96efc10ed82c51893e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b75fe51dc546fbb05f2a077caf4bf47b8213f5b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebMetric")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebMetric")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
